5de5f22decee0ad7ec96d832b9f62d07552566a9
Swayam Jain <swayamjain812@gmail.com>
2026-09-20 23:33:01 +0530

    Configure production API URL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-
-const api = axios.create({ baseURL: "/api" });
-
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL.replace(/\/$/, "")}/api`
+    : "/api",
+});
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("jp_token");
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
-
 export default api;
